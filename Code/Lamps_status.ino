121cc1201f00ec30f0e840a80deebb4f75995d53
emreozincegedik <emreozincegedik@gmail.com>
2019-11-13 07:58:10 +0300

    code snippets, interface, report

---
+++ Code/Lamps_status.ino
@@ -0,0 +1,31 @@
+#define lamp1_read A15
+#define lamp2_read A14
+void setup(){
+  Serial.begin(9600);
+  pinMode(lamp1_read, INPUT);
+  pinMode(lamp2_read, INPUT);
+}
+void loop(){
+  int lamp1_status = analogRead(lamp1_read); //0-1023
+  int lamp2_status = analogRead(lamp2_read);
+
+  if (lamp1_status>600)
+  {
+    Serial.println("Lamp1: On")
+  }
+  else
+  {
+    Serial.println("Lamp1: Off")
+  }
+
+  if (lamp2_status>600)
+  {
+    Serial.println("Lamp2: On")
+  }
+  else
+  {
+    Serial.println("Lamp2: Off")
+  }
+  
+  
+}
---
+++ Code/Soil_moisture.ino
@@ -0,0 +1,30 @@
+#define moisture_pin A13
+#define water_pump 53
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(moisture_pin, INPUT);
+pinMode(water_pump, OUTPUT);
+digitalWrite(water_pump, LOW);
+Serial.begin(9600);
+}
+char level[];
+void loop() {
+  int moisture=analogRead(moisture_pin); //moisture is 0-1023
+
+  if (moisture<200){
+    pinMode(water_pump, HIGH);
+    level="LOW";    
+    
+    }
+    else if(moisture<600){
+    level="MED";
+    }
+   else{
+    pinMode(water_pump,LOW);
+    level="HIGH";
+    }
+    Serial.print("Water level: ");
+    Serial.println(level);
+    //Also OLED screen
+}
